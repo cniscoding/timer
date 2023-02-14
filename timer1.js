37636b535aea840571b0138426055b420f294214
cniscoding <asdfa.com>
2023-02-14 14:44:41 -0800

    initial timer1

---
+++ timer1.js
@@ -0,0 +1,15 @@
+
+// No numbers are provided: Easy. It should just not beep at all and end immediately since no beeps should get scheduled.
+// An input is a negative number: Ignore/skip any numbers that are negative. We can't schedule anything in the past.
+// An input is not a number: Ignore/skip these as well, instead of attempting to call setTimeout with a non-number.
+// process.stdout.write('\x07' + ' beep');
+
+const args = process.argv.slice(2);
+console.log(args)
+
+for (let num of args){
+  num = num*1000
+  setTimeout(() => {
+    process.stdout.write('\x07' + 'beep');
+  }, num);
+}
